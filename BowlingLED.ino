c1c942970dcc8df7192de81c6a2c143a1b2f22bb
Pierrick <pcatalo@gmail.com>
2020-09-07 19:01:46 +0100

    Create BowlingLED.ino
    added file

---
+++ BowlingLED.ino
@@ -0,0 +1,212 @@
+#include "FastLED.h"
+#include <EEPROM.h>
+#define NUM_LEDS 241
+CRGB leds[NUM_LEDS];
+#define PIN 8
+#define PINARD 9
+#define BRIGHTNESS 96
+#define FRAMES_PER_SECOND  120
+
+int trigPin1 = 2;
+int echoPin1 = 3;
+int trigPin2 = 4;
+int echoPin2 = 5;
+int trigPin3 = 6;
+int echoPin3 = 7;
+int BUTTON1 = 10;
+
+uint8_t gHue = 0;
+uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
+
+bool gameMode = false;
+
+void setup() {
+  FastLED.addLeds<WS2811, PIN, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<WS2811, PINARD, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+
+  pinMode(2, INPUT_PULLUP); // internal pull-up resistor
+
+  Serial.begin (9600);
+  pinMode(trigPin1, OUTPUT);
+  pinMode(echoPin1, INPUT);
+  pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+  pinMode(trigPin3, OUTPUT);
+  pinMode(echoPin3, INPUT);
+
+  pinMode(BUTTON1, INPUT_PULLUP);
+
+  FastLED.setBrightness(BRIGHTNESS);
+}
+
+typedef void (*SimplePatternList[])();
+SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
+
+
+void loop() {
+
+  checkButton();
+
+  if (gameMode == true) {
+    long duration1, distance1, duration2, distance2, duration3, distance3;
+
+    distance1 = getDistance(trigPin1, echoPin1);
+    distance2 = getDistance(trigPin2, echoPin2);
+    distance3 = getDistance(trigPin3, echoPin3);
+
+    Serial.print("distance1 = " );
+    Serial.println(distance1);
+    Serial.print("distance2 = " );
+    Serial.println(distance2);
+    Serial.print("distance3 = " );
+    Serial.println(distance3);
+
+    if (distance1 > 90 && distance2 > 90 && distance3 > 90) {
+      Strobe(0, 0xff, 0, 10, 50, 0);
+    }
+    else {
+      bpm();
+      FastLED.show();
+      FastLED.delay(1000 / 120);
+    }
+    delay(500);
+  }
+  else {
+    gPatterns[gCurrentPatternNumber]();
+
+    FastLED.show();
+    FastLED.delay(1000 / FRAMES_PER_SECOND);
+
+    EVERY_N_MILLISECONDS( 20 ) {
+      gHue++;
+    }
+    EVERY_N_SECONDS( 10 ) {
+      nextPattern();
+    }
+  }
+}
+
+void checkButton() {
+  if (digitalRead(BUTTON1) == LOW) {
+    gameMode = !gameMode;
+    delay(500);
+  }
+}
+
+long getDistance(int trigPin, int echoPin) {
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  long duration = pulseIn(echoPin, HIGH);
+  long distance = ((duration) / 2) / 29.1;
+
+  return distance;
+}
+
+void Strobe(byte red, byte green, byte blue, int StrobeCount, int FlashDelay, int EndPause) {
+  for (int j = 0; j < StrobeCount; j++) {
+    setAll(red, green, blue);
+    showStrip();
+    delay(FlashDelay);
+    setAll(0, 0, 0);
+    showStrip();
+    delay(FlashDelay);
+  }
+
+  delay(EndPause);
+}
+
+void showStrip() {
+  // Apply LED color changes
+#ifdef ADAFRUIT_NEOPIXEL_H
+  // NeoPixel
+  strip.show();
+#endif
+#ifndef ADAFRUIT_NEOPIXEL_H
+  // FastLED
+  FastLED.show();
+#endif
+}
+
+void setPixel(int Pixel, byte red, byte green, byte blue) {
+  // Set a LED color (not yet visible)
+#ifdef ADAFRUIT_NEOPIXEL_H
+  // NeoPixel
+  strip.setPixelColor(Pixel, strip.Color(red, green, blue));
+#endif
+#ifndef ADAFRUIT_NEOPIXEL_H
+  // FastLED
+  leds[Pixel].r = red;
+  leds[Pixel].g = green;
+  leds[Pixel].b = blue;
+#endif
+}
+
+void setAll(byte red, byte green, byte blue) {
+  // Set all LEDs to a given color and apply it (visible)
+  for (int i = 0; i < NUM_LEDS; i++ ) {
+    setPixel(i, red, green, blue);
+  }
+  showStrip();
+}
+
+#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
+
+void nextPattern(){
+  // add one to the current pattern number, and wrap around at the end
+  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
+}
+
+void rainbow(){
+  // FastLED's built-in rainbow generator
+  fill_rainbow( leds, NUM_LEDS, gHue, 7);
+}
+
+void rainbowWithGlitter(){
+  // built-in FastLED rainbow, plus some random sparkly glitter
+  rainbow();
+  addGlitter(80);
+}
+
+void addGlitter( fract8 chanceOfGlitter){
+  if ( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB::White;
+  }
+}
+
+void confetti(){
+  // random colored speckles that blink in and fade smoothly
+  fadeToBlackBy( leds, NUM_LEDS, 10);
+  int pos = random16(NUM_LEDS);
+  leds[pos] += CHSV( gHue + random8(64), 200, 255);
+}
+
+void sinelon(){
+  // a colored dot sweeping back and forth, with fading trails
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  int pos = beatsin16( 13, 0, NUM_LEDS - 1 );
+  leds[pos] += CHSV( gHue, 255, 192);
+}
+
+void bpm(){
+  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
+  uint8_t BeatsPerMinute = 62;
+  CRGBPalette16 palette = HeatColors_p;
+  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
+  for ( int i = 0; i < NUM_LEDS; i++) { //9948
+    leds[i] = ColorFromPalette(palette, gHue + (i * 2), beat - gHue + (i * 10));
+  }
+}
+
+void juggle() {
+  // eight colored dots, weaving in and out of sync with each other
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  byte dothue = 0;
+  for ( int i = 0; i < 8; i++) {
+    leds[beatsin16( i + 7, 0, NUM_LEDS - 1 )] |= CHSV(dothue, 200, 255);
+    dothue += 32;
+  }
+}
